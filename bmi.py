21aed7fa2f1095e0f4f171ce89340edb072a685c
Lenny <151894957+lennynGuyen24@users.noreply.github.com>
2024-03-21 15:19:40 -0400

    Add files via upload
    Body Mass Index (BMI) Calculator 
    BMI is a person’s weight in kilograms divided by the square of height in meters. A high BMI can indicate high body fatness.  https://en.wikipedia.org/wiki/Body_mass_index
    *If your BMI is less than 18.5, it falls within the underweight range.
    *If your BMI is 18.5 to <25, it falls within the healthy weight range.
    *If your BMI is 25.0 to <30, it falls within the overweight range.
    *If your BMI is 30.0 or higher, it falls within the obesity range.

---
+++ bmi.py
@@ -0,0 +1,15 @@
+print("Enter your height in meters, e.g. 1.8 \n")
+height =float(input())
+print("Enter your weight in kilograms, e.g. 70 \n")
+weight = int(input())
+bmi = float(weight/ (height * height))
+if bmi < 18.5:
+  print(f"Your BMI is {bmi}, you are underweight.")
+elif bmi < 25:
+  print(f"Your BMI is {bmi}, you have a normal weight.")
+elif bmi <30:
+  print(f"Your BMI is {bmi}, you are slightly overweight.")
+elif bmi <35:
+  print(f"Your BMI is {bmi}, you are obese.")
+else:
+  print(f"Your BMI is {bmi}, you are clinically obese.")
